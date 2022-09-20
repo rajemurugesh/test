30df1160e2749cbd61aa9a92788ca647510f1220
rajemurugesh <82995662+rajemurugesh@users.noreply.github.com>
2022-09-20 19:35:22 +0530

    aassasa

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res)=>{
     res.send('hello world')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
